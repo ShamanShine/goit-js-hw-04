6a46cc6e5480123c2efa43b15f91f508e5442c9c
ShamanShine <lazun2016@gmail.com>
2023-12-14 22:41:57 +0200

    add code task#2. start

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,4 @@
 function isEnoughCapacity(products, containerSize) {
-
   let totalProducts = 0;
 
   // нужно перебрать объект посчитать количество товаров
@@ -7,23 +6,13 @@ function isEnoughCapacity(products, containerSize) {
     totalProducts += products[key];
   }
 
-  return totalProducts <= containerSize;  // Сравниваем количество товаров с размером контейнера и возвращаем результат сравнения
+  return totalProducts <= containerSize; // Сравниваем количество товаров с размером контейнера и возвращаем результат сравнения
 }
 
+console.table(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 
-console.log(
-  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-); // true
+console.table(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
 
-console.log(
-  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-); // false
+console.table(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
 
-console.log(
-  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
-); // true
-
-console.log(
-  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
-); // false
-
+console.table(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
